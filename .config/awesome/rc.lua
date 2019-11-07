46e7f280599537784614b0589a845ff63a53e117
Daniel Lucas <dlsbdaniel@gmail.com>
2019-11-07 16:32:10 -0300

    Changing border size

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -274,7 +274,7 @@ screen.connect_signal("arrange", function (s)
     local only_one = #s.tiled_clients == 1
     for _, c in pairs(s.clients) do
         if only_one and not c.floating or c.maximized then
-            c.border_width = 2
+            c.border_width = 0
         else
             c.border_width = beautiful.border_width
         end
